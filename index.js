e04be8a1d2f7fd2450f3f678d0f3ba8683c440fa
Maximilian Dewald <maximilian@dewaldnet.de>
2023-12-02 13:40:24 +0100

    update to strict rules

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:@typescript-eslint/strict",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
         "plugin:compat/recommended",
         "plugin:import/errors",
         "plugin:import/warnings",
